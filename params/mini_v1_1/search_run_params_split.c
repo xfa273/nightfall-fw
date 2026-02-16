4f6142b97db26ba3283aa1a712969d841350228f
Yuho Nagasaki <xfa273@M4MBA.local>
2026-02-16 18:33:20 +0900

    tune(mini): adjust search val_offset_in for case1

--- params/mini_v1_1/search_run_params_split.c
+++ params/mini_v1_1/search_run_params_split.c
@@ -14,7 +14,7 @@ const SearchRunParams_t searchRunParams[2] = {
         .acceleration_turn = 0.0f,
         .dist_offset_in = 10.0f,
         .dist_offset_out = 14.5f,
-        .val_offset_in = 650.0f,
+        .val_offset_in = 630.0f,
         .angle_turn_90 = 90.0f,
         
         // 壁切れ後の追従距離
